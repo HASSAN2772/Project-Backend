05528e330541d18f1da29e3221e9482a6cabf8ef
Ramzan afzal <ramzanafzal82@gmail.com>
2023-03-21 23:56:15 +0500

    finalize

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 const express = require("express");
-const mongoose = require("mongoose")
-const app = express()
-const cors =  require("cors")
-const bodyParser = require('body-parser');
-const cookieParser = require("cookie-parser")
-const router =  require("./Routes/userRoutes")
-const mongodb = require("./Database/database")
-const dotenv = require("dotenv")
-const path =  require("path")
-require('dotenv').config();
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const router = require("./Routes/userRoutes");
+const mongodb = require("./Database/database");
+const dotenv = require("dotenv");
+const path = require("path");
+require("dotenv").config();
 app.use(express.json());
 app.use(cors());
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // importing  routes
-app.use('/api/v1',router)
+app.use("/api/v1", router);
 
-
-
-
-
-app.listen(process.env.PORT,()=>
-{
-    console.log( `Server is Listening on Port ${process.env.PORT}`)    
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server is Listening on Port ${process.env.PORT}`);
+});
